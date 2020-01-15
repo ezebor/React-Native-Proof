3f9a3218107ac654d9e7a986961ba5259c8fdb80
Ezequiel Borenstein <ezequielborenstein@MacBook-Pro-de-Ezequiel.local>
2020-01-15 15:03:35 -0300

    Moved folder

---
+++ App.js
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  Image,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text>HALAAAAAAAA</Text>
+      <Image 
+        source={require('./assets/logo.png')}
+        style={{width:300, height:80}}
+      />
+    </View>
+  );
+};
+
+export default App;
